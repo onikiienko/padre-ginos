2194b1df856ec3836966b38e97249844068000dc
Onikiienko Bogdan <onikiienko@macpaw.com>
2024-12-04 19:48:51 +0200

    feat: setup pizza server

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's</h1>
-      <Pizza name="Pepperoni" description="A classic pizza with pepperoni"/>
-      <Pizza name="Margherita" description="A classic pizza with tomato and mozzarella"/>
-      <Pizza name="Quattro Stagioni" description="A classic pizza with ham, mushrooms, artichokes, and olives"/>
-      <Pizza name="Quattro Formaggi" description="A classic pizza with four types of cheese"/>
-      <Pizza name="Capricciosa" description="A classic pizza with ham, mushrooms, artichokes, and olives"/>
+      <Pizza name="Pepperoni" description="A classic pizza with pepperoni" image="/public/pizzas/big_meat.webp" />
+      <Pizza name="Margherita" description="A classic pizza with tomato and mozzarella" image="/public/pizzas/hawaiian.webp" />
+      <Pizza name="Quattro Stagioni" description="A classic pizza with ham, mushrooms, artichokes, and olives"  image="/public/pizzas/big_meat.webp"/>
+      <Pizza name="Quattro Formaggi" description="A classic pizza with four types of cheese"  image="/public/pizzas/big_meat.webp"/>
+      <Pizza name="Capricciosa" description="A classic pizza with ham, mushrooms, artichokes, and olives"  image="/public/pizzas/big_meat.webp"/>
     </div>
   );
 };
