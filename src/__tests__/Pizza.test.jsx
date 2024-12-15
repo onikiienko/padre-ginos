5ac83c5ffd3a7b18a10a143dd52a8e92e27e0132
Onikiienko Bogdan <onikiienko@macpaw.com>
2024-12-15 17:57:19 +0200

    test: TDD

--- src/__tests__/Pizza.test.jsx
+++ src/__tests__/Pizza.test.jsx
@@ -1,6 +1,9 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
 import Pizza from "../Pizza";
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
 
 test("alt text renders on Pizza image", async () => {
   const name = "My favorite pizza";
@@ -12,4 +15,13 @@ test("alt text renders on Pizza image", async () => {
   const img = screen.getByRole("img");
   expect(img.src).toBe(src);
   expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name="something else" description="super cool pizza" />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
 });
